ad9436dd584b81504cae7b41fcf3b0123f3090d2
Prakashgunasekaran <prakash.gunasekaran@sada.com>
2025-02-04 09:04:50 -0500

    added create table method for Gemini

--- modules/FHIResourceManager.py
+++ modules/FHIResourceManager.py
@@ -280,3 +280,141 @@ class FHIRResourceManager:
 
         # Return the response
         return response.content
+    
+    @log_entry_exit
+    def create_sql_from_schema_gemini(self, enriched_schema_json, table_description, mode):
+
+        generated_sql = ""
+        if mode == "alter":
+            logger.info("Creating ALTER TABLE SQL statements...")
+            prompt_template = PromptTemplate(
+                    input_variables=["schema_json", "resource_name", "full_table_name", "table_description"],
+                    template="""
+                        You are a data engineer who needs to write an ALTER TABLE query for an existing 
+                        BigQuery {resource_name} table with name: {full_table_name}.
+
+
+                        Here is the schema for the FHIR {resource_name} table. Write an **ALTER TABLE DDL** statement 
+                        to **add column descriptions** to the encounter table:
+                        {schema_json}
+
+                        Do NOT generate an ALTER COLUMN statement for nested fields.
+
+                    """)
+            
+        elif mode == "create":
+            logger.info("Creating CREATE TABLE SQL statements...")
+            prompt_template = PromptTemplate(
+                    input_variables=["schema_json", "resource_name", "full_table_name", "table_description"],
+                    template="""
+                            You are an expert in SQL and FHIR schema design. Given the following JSON schema, generate valid BigQuery SQL column definitions.
+
+                            ### **Requirements:**
+                            1. **Each field must be properly typed** according to BigQuery SQL conventions.
+                            2. **Descriptions in OPTIONS(description="...") must always be enclosed in double quotes**.
+                            - **Ensure that the description starts and ends with a double quote (`"`).**
+                            - **Never leave a description without a closing quote.**
+                            - **If a description ends  with a closing ), ensure that the description ends with a double quote (`"`).**
+                            3. **Do NOT generate `CREATE OR REPLACE TABLE`, only column definitions.**
+                            4. **Output must be a valid SQL list of column definitions.**
+                            5. **Rephrase descriptions that are more than 1024 characters.**
+                            6. **For "REPEATED" mode and "RECORD" type columns, use valid BigQuery SQL syntax.**
+                            7. **DO NOT enclose the output with triple backticks.**
+
+                            ### **Schema enclosed with ```:**
+                           ```{schema_json}```
+
+                            #### **Input Example 1:**
+                            {{
+                                "name": "encounter_id",
+                                "mode": "",
+                                "type": "STRING",
+                                "description": "The 'encounter_id' field represents the unique identifier for a specific Encounter resource within the FHIR system. This is typically a UUID or a system-specific identifier. Key Considerations: Data Type: String, allowing for alphanumeric characters and special symbols. Uniqueness: Must be unique within the scope of the FHIR server or system. Clinical Significance: Used to reference a specific encounter in other resources and systems. Essential for tracking and managing patient encounters. Relationship to Other Resources: Used as a foreign key in other resources that reference the encounter. Special Considerations: Should be generated using a consistent method to ensure uniqueness. In Summary: The 'encounter_id' is a fundamental element for identifying and referencing a specific patient encounter within a FHIR environment.",
+                                "fields": []
+                            }}
+                            
+                            #### **Output Example 1:**
+                            encounter_id STRING OPTIONS(description="The 'encounter_id' field represents the unique identifier for a specific Encounter resource.)",
+
+                            #### **Input Example 2:**
+                            {{
+                            "name": "registration_query",
+                            "mode": "REPEATED",
+                            "type": "RECORD",
+                            "description": "The 'registration_query' field is a repeated record containing details about questions asked during the patient registration process and their corresponding responses. This field is used to capture structured data related to patient intake and demographics. It is a repeated structure, allowing for multiple questions and answers to be recorded.",
+                            "fields": [
+                              {{
+                                "name": "mnemonic",
+                                "mode": "",
+                                "type": "STRING",
+                                "description": "The 'mnemonic' field represents a short, memorable code or identifier for a specific question asked during the patient registration process. This mnemonic is used to quickly identify the question and its associated response. It is typically a string value. This field is essential for efficient data retrieval and analysis.",
+                                "fields": []
+                              }},
+                              {{
+                                "name": "question",
+                                "mode": "",
+                                "type": "STRING",
+                                "description": "The 'question' field represents the actual text of the question asked during the patient registration process. This field provides a human-readable representation of the question. It is typically a string value. This field is essential for understanding the context of the response.",
+                                "fields": []
+                              }},
+                              {{
+                                "name": "response",
+                                "mode": "",
+                                "type": "STRING",
+                                "description": "The 'response' field represents the patient's answer to the corresponding question asked during the registration process. This field captures the patient's input. It is typically a string value. This field is essential for collecting patient information during registration.",
+                                "fields": []
+                              }}
+                              ]
+                              }}
+                            
+                            #### **Output Example 2:**
+                            registration_query ARRAY<STRUCT<
+                                    mnemonic STRING OPTIONS(description="The 'mnemonic' field represents a short, memorable code or identifier for a specific question asked during the patient registration process. This mnemonic is used to quickly identify the question and its associated response. It is typically a string value. This field is essential for efficient data retrieval and analysis."),
+                                    question STRING OPTIONS(description="The 'question' field represents the actual text of the question asked during the patient registration process. This field provides a human-readable representation of the question. It is typically a string value. This field is essential for understanding the context of the response."),
+                                    response STRING OPTIONS(description="The 'response' field represents the patient's answer to the corresponding question asked during the registration process. This field captures the patient's input. It is typically a string value. This field is essential for collecting patient information during registration.")
+                                >> OPTIONS(description="The 'registration_query' field is a repeated record containing details about questions asked during the patient registration process and their corresponding responses. This field is used to capture structured data related to patient intake and demographics. It is a repeated structure, allowing for multiple questions and answers to be recorded."),
+                    """)
+        else:
+            logger.error(f"Invalid mode specified: {mode}")
+            pass
+        
+        # Define chunk size (e.g., process 10 fields at a time)
+        try: 
+            chunk_size = 10
+            logger.info(f"Splitting schema into chunks of {chunk_size} fields...")
+            schema_chunks = [enriched_schema_json[i:i + chunk_size] for i in range(0, len(enriched_schema_json), chunk_size)]
+
+            generated_sql = []
+        
+            # Initialize SQL script with only the `CREATE OR REPLACE TABLE` statement
+            generated_sql = f"CREATE OR REPLACE TABLE {self.full_table_name} (\n"
+
+            for idx, chunk in enumerate(schema_chunks):
+                logger.info(f"Processing chunk {idx + 1}/{len(schema_chunks)}...")
+
+                # Format the prompt with the chunk
+                prompt = prompt_template.format(
+                    schema_json=json.dumps(chunk, indent=2),
+                    full_table_name=self.full_table_name, 
+                    table_description=table_description,
+                    resource_name=self.fhir_resource_name)
+
+                # Create a message array containing the formatted prompt
+                messages = [HumanMessage(content=prompt)]
+                
+                # Send request to LLM
+                response = self.llm_model.invoke(input=messages)
+
+                if idx!=0:
+                    generated_sql+=f",\n    {response.content}"
+                else:
+                    generated_sql+=f"    {response.content}"
+
+            # Close the SQL statement with `OPTIONS(...)`
+            generated_sql += "\n)\nOPTIONS(\n  description='FHIR Encounter Table'\n);"
+
+        except json.JSONDecodeError as e:
+            logger.error(f"Error parsing JSON for chunk {idx + 1}. error:{e}")
+
+        logger.info("Creation of SQL completed successfully...")
+        return generated_sql
